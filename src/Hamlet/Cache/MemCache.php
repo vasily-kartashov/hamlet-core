6aabf8e5b1576d67541251cd4ea1ebf7165a5007
Vasily Kartashov <info@kartashov.com>
2014-07-04 22:03:21 +0100

    memcached updated

--- src/Hamlet/Cache/MemCache.php
+++ src/Hamlet/Cache/MemCache.php
@@ -6,42 +6,72 @@ use Memcached;
 
 class MemCache implements CacheInterface
 {
-    /** @var string */
-    protected $endpoint;
+    /** @var array {
+     *      string $host
+     *      int $port
+     * }
+     */
+    protected $endpoints;
 
-    /** @var int */
-    protected $port;
-
-    /** @var \Memcached */
-    protected $client = null;
-
-    public function __construct($endpoint, $port = 11211)
+    /**
+     * @param $endpoints array {
+     *      string $host
+     *      int $port
+     * }
+     */
+    public function __construct(array $endpoints)
     {
-        $this->endpoint = $endpoint;
-        $this->port = $port;
+        $this->endpoints = $endpoints;
     }
 
+    /**
+     * Get memcached client
+     *
+     * @return \Memcached
+     */
+    private function getClient()
+    {
+        static $client = null;
+        if ($client == null) {
+            $client = new Memcached();
+            foreach ($this->endpoints as $endpoint) {
+                $client->addServer($endpoint['host'], $endpoint['port']);
+            }
+        }
+        return $client;
+    }
+
+    /**
+     * Get value stored in cache
+     *
+     * @param string $key
+     * @param mixed $defaultValue
+     *
+     * @return array
+     */
     public function get($key, $defaultValue = null)
     {
-        if ($this->client == null) {
-            $this->client = new Memcached();
-            $this->client->addServer($this->endpoint, $this->port);
-        }
+        $client = $this->getClient();
         $found = true;
-        $value = $this->client->get($key);
-        if ($this->client->getResultCode() == Memcached::RES_NOTFOUND) {
+        $value = $client->get($key);
+        if ($client->getResultCode() == Memcached::RES_NOTFOUND) {
             $found = false;
             $value = $defaultValue;
         }
         return [$value, $found];
     }
 
+    /**
+     * Store value in cache
+     *
+     * @param string $key
+     * @param mixed $value
+     *
+     * @param int $timeToLive
+     */
     public function set($key, $value, $timeToLive = 0)
     {
-        if ($this->client == null) {
-            $this->client = new Memcached();
-            $this->client->addServer($this->endpoint, $this->port);
-        }
-        $this->client->set($key, $value, $timeToLive);
+        $client = $this->getClient();
+        $client->set($key, $value, $timeToLive);
     }
 }
