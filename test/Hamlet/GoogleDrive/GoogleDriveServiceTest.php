14ba004114583ce2cf4a882b908ea0030020a0c6
Vasily Kartashov <info@kartashov.com>
2014-07-09 08:20:57 +0100

    need to read the credentials from a secret location, not like this

--- test/Hamlet/GoogleDrive/GoogleDriveServiceTest.php
+++ test/Hamlet/GoogleDrive/GoogleDriveServiceTest.php
@@ -11,7 +11,7 @@ class GoogleDriveServiceTest extends UnitTestCase
 
     public function __construct()
     {
-        $path = realpath(__DIR__ . '/../../../.credentials/google.json');
+        $path = realpath('~/.hamlet/google.json');
         $credentials = json_decode(file_get_contents($path));
 
         $factory = new GoogleDriveClientFactory();
