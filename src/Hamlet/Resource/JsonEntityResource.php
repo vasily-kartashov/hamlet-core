ee95dd1219fce9b9e9c2d135a3810f5ee61e6bf7
Danny Marsland <danny.marsland@etecture.co.uk>
2014-07-17 14:50:59 +0100

    fixing JsonEntityResource allowed methods

--- src/Hamlet/Resource/JsonEntityResource.php
+++ src/Hamlet/Resource/JsonEntityResource.php
@@ -10,8 +10,8 @@ class JsonEntityResource extends EntityResource
      * @param mixed $value
      * @param string[] $methods
      */
-    public function __construct($value, $methods = ['GET'])
+    public function __construct($value, $methods = ['GET', 'POST'])
     {
-        parent::__construct(new JsonEntity($value));
+        parent::__construct(new JsonEntity($value), $methods);
     }
 }
