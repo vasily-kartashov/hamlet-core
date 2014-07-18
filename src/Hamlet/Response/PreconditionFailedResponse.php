140e1e6d347cfaf0c7425acd137a033b690e80a7
Danny Marsland <danny.marsland@etecture.co.uk>
2014-07-18 10:58:45 +0100

    Adding entity to precondition failed

--- src/Hamlet/Response/PreconditionFailedResponse.php
+++ src/Hamlet/Response/PreconditionFailedResponse.php
@@ -2,10 +2,15 @@
 
 namespace Hamlet\Response;
 
+use Hamlet\Entity\EntityInterface;
+
 class PreconditionFailedResponse extends AbstractResponse
 {
-    public function __construct()
+    public function __construct(EntityInterface $entity = null)
     {
         parent::__construct('412 Precondition Failed');
+        if (!is_null($entity)) {
+            $this->setEntity($entity);
+        }
     }
 }
