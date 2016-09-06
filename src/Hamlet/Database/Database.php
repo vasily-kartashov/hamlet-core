96090a0e9b08ea329fbb549b9f28ddb64f0fa5ce
Vasily Kartashov <info@kartashov.com>
2016-09-06 17:53:46 +1000

    database factory added

---
+++ src/Hamlet/Database/Database.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Hamlet\Database {
+
+    use Exception;
+    use mysqli;
+    use SQLite3;
+
+    class Database {
+
+        private $connection;
+        private $type;
+
+        private function __construct($connection, $type) {
+            $this -> connection = $connection;
+            $this -> type = $type;
+        }
+
+        public static function mysql($host, $user, $password, $database = null) {
+            $connection = new mysqli($host, $user, $password, $database);
+            return new Database($connection, 'mysql');
+        }
+
+        public static function sqlite($path) {
+            $connection = new SQLite3($path);
+            return new Database($connection, 'sqlite');
+        }
+
+        public function procedure($query) {
+            switch ($this -> type) {
+                case 'mysql':
+                    return new MySQLProcedure($this -> connection, $query);
+                case 'sqlite':
+                    return new SQLiteProcedure($this -> connection, $query);
+                default:
+                    throw new Exception("Unsupported database type {$this -> type}");
+            }
+        }
+    }
+}
