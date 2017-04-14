7536004aa03365535152d014c0a7aa711bdbcbfd
Vasily Kartashov <info@kartashov.com>
2017-04-14 23:10:21 +1000

    ignore unmapped properties

--- src/Hamlet/Database/MappedEntity.php
+++ src/Hamlet/Database/MappedEntity.php
@@ -8,13 +8,17 @@ namespace Hamlet\Database {
 
         /**
          * @param array $properties
+         * @param bool $ignoreUnmapped
          * @return static
          */
-        public static function from(array $properties) {
+        public static function from(array $properties, bool $ignoreUnmapped = true) {
             $type = new ReflectionClass(static::class);
             $object = new static();
             foreach ($properties as $key => $value) {
                 $name = lcfirst(str_replace('_', '', ucwords($key, '_')));
+                if ($ignoreUnmapped && !$type->hasProperty($name)) {
+                    continue;
+                }
                 $property = $type->getProperty($name);
                 $property->setAccessible(true);
                 $property->setValue($object, $value);
