771a14feaca69a8c40cd4cf5e5b292f7c9a66bbe
Vasily Kartashov <info@kartashov.com>
2017-04-15 19:32:20 +1000

    no translation for property name in mapped entities, check equivalence

--- src/Hamlet/Database/MappedEntity.php
+++ src/Hamlet/Database/MappedEntity.php
@@ -2,26 +2,34 @@
 
 namespace Hamlet\Database {
 
+    use Exception;
     use ReflectionClass;
+    use ReflectionProperty;
 
     abstract class MappedEntity {
 
         /**
-         * @param array $properties
+         * @param array $data
          * @param bool $ignoreUnmapped
          * @return static
+         * @throws Exception
          */
-        public static function from(array $properties, bool $ignoreUnmapped = true) {
+        public static function from(array $data, bool $ignoreUnmapped = false) {
             $type = new ReflectionClass(static::class);
             $object = new static();
-            foreach ($properties as $key => $value) {
-                $name = lcfirst(str_replace('_', '', ucwords($key, '_')));
+            foreach ($data as $name => $value) {
                 if ($ignoreUnmapped && !$type->hasProperty($name)) {
                     continue;
                 }
                 $property = $type->getProperty($name);
                 $property->setAccessible(true);
                 $property->setValue($object, $value);
+            }
+            foreach ($type->getProperties(ReflectionProperty::IS_PRIVATE) as $property) {
+                if (!isset($data[$property->getName()])) {
+                    throw new Exception('Property ' . $type->getName() . '::' . $property->getName()
+                        . ' not set in ' . json_encode($data));
+                }
             }
             return $object;
         }
