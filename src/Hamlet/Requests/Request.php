e01e11caa0969b15fe035b6578c75c6d2b356cc1
Vasily Kartashov <info@kartashov.com>
2017-05-02 22:57:52 +1000

    stupid guzzle

--- src/Hamlet/Requests/Request.php
+++ src/Hamlet/Requests/Request.php
@@ -3,12 +3,32 @@
 namespace Hamlet\Requests;
 
 use DateTime;
+use GuzzleHttp\Psr7\LazyOpenStream;
 use GuzzleHttp\Psr7\ServerRequest;
 use Hamlet\Cache\Cache;
 use Hamlet\Entities\Entity;
 
 class Request extends ServerRequest
 {
+    public static function fromGlobals()
+    {
+        $method = isset($_SERVER['REQUEST_METHOD']) ? $_SERVER['REQUEST_METHOD'] : 'GET';
+        $headers = function_exists('getallheaders') ? getallheaders() : [];
+        $uri = self::getUriFromGlobals();
+        $body = new LazyOpenStream('php://input', 'r+');
+        $protocol = isset($_SERVER['SERVER_PROTOCOL'])
+            ? str_replace('HTTP/', '', $_SERVER['SERVER_PROTOCOL'])
+            : '1.1';
+
+        $serverRequest = new Request($method, $uri, $headers, $body, $protocol, $_SERVER);
+
+        return $serverRequest
+            ->withCookieParams($_COOKIE)
+            ->withQueryParams($_GET)
+            ->withParsedBody($_POST)
+            ->withUploadedFiles(self::normalizeFiles($_FILES));
+    }
+
     public function getEnvironmentName(): string
     {
         return $this->getServerParams()['SERVER_NAME'];
