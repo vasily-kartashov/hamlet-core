c041f46a3df044af692dd585bd7c49907d53c4b1
Vasily Kartashov <info@kartashov.com>
2017-05-02 13:14:55 +1000

    better hint for phpstorm

--- src/Hamlet/Resources/JsonEntityResource.php
+++ src/Hamlet/Resources/JsonEntityResource.php
@@ -6,6 +6,11 @@ namespace Hamlet\Resources {
 
     class JsonEntityResource extends EntityResource {
 
+        /**
+         * JsonEntityResource constructor.
+         * @param mixed $value
+         * @param string[] $methods
+         */
         public function __construct($value, array $methods = ['GET', 'POST']) {
             parent::__construct(new JsonEntity($value), $methods);
         }
