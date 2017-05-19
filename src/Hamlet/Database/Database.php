2965dec221db26550684f3b19ad8318b40eb2a46
Vasily Kartashov <info@kartashov.com>
2017-05-19 10:41:07 +1000

    better way to nest transactions

--- src/Hamlet/Database/Database.php
+++ src/Hamlet/Database/Database.php
@@ -2,12 +2,14 @@
 
 namespace Hamlet\Database;
 
+use Exception;
 use Hamlet\Database\MySQL\MySQLDatabase;
 use mysqli;
 
 abstract class Database
 {
     protected $logger;
+    protected $transactionStarted = false;
 
     public static function mysql(string $host, string $user, string $password, string $database = null): Database {
         $connection = new mysqli($host, $user, $password, $database);
@@ -21,4 +23,23 @@ abstract class Database
     public abstract function commit();
 
     public abstract function rollback();
+
+    public function withTransaction(callable $callable)
+    {
+        try {
+            $nested = $this->transactionStarted;
+            if (!$nested) {
+                $this->startTransaction();
+            }
+            $this->transactionStarted = true;
+            $callable();
+            if (!$nested) {
+                $this->commit();
+            }
+            $this->transactionStarted = $nested;
+        } catch (Exception $e) {
+            $this->rollback();
+            throw $e;
+        }
+    }
 }
