d8e13fc710fcd15dc657f7726fa9c3a077f00e36
Vasily Kartashov <vasily.kartashov@hunterindustries.com>
2018-10-22 10:06:29 +1100

    using different server

--- src/Hamlet/Bootstraps/SwooleBootstrap.php
+++ src/Hamlet/Bootstraps/SwooleBootstrap.php
@@ -8,7 +8,6 @@ use Hamlet\Writers\SwooleResponseWriter;
 use Swoole\Http\Request as SwooleRequest;
 use Swoole\Http\Response as SwooleResponse;
 use Swoole\Http\Server;
-use Throwable;
 
 final class SwooleBootstrap
 {
@@ -24,18 +23,16 @@ final class SwooleBootstrap
      */
     public static function run(string $host, int $port, AbstractApplication $application)
     {
-        $server = new Server($host, $port);
+        $server = new Server($host, $port, SWOOLE_BASE);
+        $server->set([
+            'worker_num' => (int) shell_exec('grep -c processor /proc/cpuinfo')
+        ]);
 
         $server->on('request', function (SwooleRequest $swooleRequest, SwooleResponse $swooleResponse) use ($application) {
-            try {
-                $request = Request::fromSwooleRequest($swooleRequest, $application->sessionHandler());
-                $writer = new SwooleResponseWriter($swooleResponse, $application->sessionHandler());
-                $response = $application->run($request);
-                $application->output($request, $response, $writer);
-            } catch (Throwable $e) {
-                print_r($e);
-                error_log($e->getMessage());
-            }
+            $request = Request::fromSwooleRequest($swooleRequest, $application->sessionHandler());
+            $writer = new SwooleResponseWriter($swooleResponse, $application->sessionHandler());
+            $response = $application->run($request);
+            $application->output($request, $response, $writer);
         });
         $server->start();
     }
