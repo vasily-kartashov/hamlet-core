6f672b937637d6a34fdbe330226b3948dadb8a81
Vasily Kartashov <info@kartashov.com>
2020-04-17 10:33:41 +1000

    psalm fixes

--- src/Hamlet/Database/MySQL/MySQLException.php
+++ src/Hamlet/Database/MySQL/MySQLException.php
@@ -9,6 +9,6 @@ class MySQLException extends RuntimeException
 {
     public function __construct(mysqli $connection)
     {
-        parent::__construct($connection->error ?? 'Unknown error', $connection->errno ?? -1);
+        parent::__construct($connection->error, $connection->errno);
     }
 }
